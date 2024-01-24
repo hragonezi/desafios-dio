b9c7869442c8832ad90f5d45c706809f6506003f
Horácio Ragonezi <horacioragonezi@gmail.com>
2024-01-24 19:43:32 -0300

    Desafio Java - Simulação de conta

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        try (Scanner scan = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Por favor, digite o número da Agência !");
+            agencia = scan.next();
+
+            System.out.println("Por favor, digite o número da Conta!");
+            numero = scan.nextInt();
+
+            System.out.println("Por favor, digite o seu nome!");
+            nomeCliente = scan.next();
+
+            System.out.println("Por favor, informe seu saldo atual");
+            saldo = scan.nextDouble();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        }
+    }
+}
